06a26e1d385db2c33841cdc2cc2538fe283f9c5b
sasca37 <sasca37@naver.com>
2023-06-03 00:43:07 +0900

    init project

---
+++ src/main/java/com/dodal/meet/DodalApplication.java
@@ -0,0 +1,13 @@
+package com.dodal.meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodalApplication.class, args);
+    }
+
+}
