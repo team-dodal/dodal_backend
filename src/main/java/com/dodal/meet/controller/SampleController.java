fbb34bfaf6ea7b9e83a847967284923a0bfe0671
sasca37 <sasca37@naver.com>
2023-06-04 20:08:20 +0900

    swagger init

--- src/main/java/com/dodal/meet/controller/SampleController.java
+++ src/main/java/com/dodal/meet/controller/SampleController.java
@@ -1,6 +1,10 @@
 package com.dodal.meet.controller;
 
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
     @GetMapping("/")
+    @ApiOperation(value = "서버 profile 확인", notes = "EC2 서버 프로필 정보 확인 (dev, prod)")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "정상"),
+            @ApiResponse(code = 500, message = "서버오류")
+    })
     public String sample(){
         String profile = System.getProperty("spring.profiles.active");
         return "현재 서버는 " + profile +" 모드입니다.";
