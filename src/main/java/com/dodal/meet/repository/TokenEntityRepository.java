8852b5c60bdb76374a8f6d20812c723d4befbffa
sasca37 <sasca37@naver.com>
2023-06-25 22:45:11 +0900

    #15 - 유저 엔티티 토큰 생성 분리

---
+++ src/main/java/com/dodal/meet/repository/TokenEntityRepository.java
@@ -0,0 +1,11 @@
+package com.dodal.meet.repository;
+
+import com.dodal.meet.model.entity.TokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TokenEntityRepository extends JpaRepository<TokenEntity, Long> {
+
+}
