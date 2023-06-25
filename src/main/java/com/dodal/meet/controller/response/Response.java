f7d7949026fa7d525d0d4d18cb482cf6349aa119
sasca37 <sasca37@naver.com>
2023-06-25 23:38:07 +0900

    #17 - Response 반환 포맷 변경 (카멜 케이스 -> 스네이크 케이스)

--- src/main/java/com/dodal/meet/controller/response/Response.java
+++ src/main/java/com/dodal/meet/controller/response/Response.java
@@ -4,6 +4,8 @@ package com.dodal.meet.controller.response;
 import com.dodal.meet.controller.response.user.UserSignInResponse;
 import com.dodal.meet.controller.response.user.UserSignUpResponse;
 import com.dodal.meet.exception.ErrorCode;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +15,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Builder
 @Schema(description = "공통 응답 처리")
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Response<T> {
 
     @Schema(description = "응답 코드", example = "공통 응답 코드")
