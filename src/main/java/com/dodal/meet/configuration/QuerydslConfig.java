3e78936a14211de3e9b42326c360da5a52f855c3
sasca37 <sasca37@naver.com>
2023-07-24 00:47:17 +0900

    #52 - Querydsl 빈 등록

---
+++ src/main/java/com/dodal/meet/configuration/QuerydslConfig.java
@@ -0,0 +1,17 @@
+package com.dodal.meet.configuration;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class QuerydslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+}
