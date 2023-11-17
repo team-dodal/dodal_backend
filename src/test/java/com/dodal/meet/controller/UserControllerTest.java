13df67768112d0b6a357e6e661a4eb3f2a48a0e0
sasca37 <sasca37@naver.com>
2023-11-17 19:57:00 +0900

    #99 - 유저 테스트 코드작성

---
+++ src/test/java/com/dodal/meet/controller/UserControllerTest.java
@@ -0,0 +1,159 @@
+package com.dodal.meet.controller;
+
+import com.dodal.meet.controller.request.user.UserSignInRequest;
+import com.dodal.meet.controller.request.user.UserSignUpRequest;
+import com.dodal.meet.controller.request.user.UserUpdateRequest;
+import com.dodal.meet.controller.response.user.UserInfoResponse;
+import com.dodal.meet.controller.response.user.UserSignInResponse;
+import com.dodal.meet.controller.response.user.UserSignUpResponse;
+import com.dodal.meet.exception.DodalApplicationException;
+import com.dodal.meet.exception.ErrorCode;
+import com.dodal.meet.model.SocialType;
+import com.dodal.meet.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@WithMockUser
+class UserControllerTest {
+
+ /*
+    @SpringBootTest + @AutoConfigureMockMvc는 통합테스트 적용할 때 사용한다.
+    @WebMvcTest는 MVC쪽만 슬라이스(slice) 테스트
+
+ */
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+
+
+    @Test
+    void 회원가입() throws Exception {
+        SocialType socialType = SocialType.KAKAO;
+        String socialId = "231595151";
+        String email = "sasca37@naver.com";
+        String nickname = "노래하는 어피치";
+        String profileUrl = "https://aws-s3.com";
+        String content = "안녕하세요";
+        List<String> tagList = Arrays.asList("001001", "001002");
+        UserSignUpRequest request =  UserSignUpRequest
+                .builder()
+                .socialType(socialType)
+                .socialId(socialId)
+                .email(email)
+                .nickname(nickname)
+                .profileUrl(profileUrl)
+                .content(content)
+                .tagList(tagList)
+                .build();
+        when(userService.signUp(request)).thenReturn(mock(UserSignUpResponse.class));
+
+        mockMvc.perform(post("/api/v1/users/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        ).andDo(print()).andExpect(status().isCreated());
+    }
+
+    @Test
+    void 로그인() throws Exception {
+        SocialType socialType = SocialType.KAKAO;
+        String socialId = "231595151";
+        UserSignInRequest request = UserSignInRequest
+                .builder()
+                .socialType(socialType)
+                .socialId(socialId)
+                .build();
+
+        when(userService.signIn(request)).thenReturn(mock(UserSignInResponse.class));
+
+        mockMvc.perform(post("/api/v1/users/sign-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        ).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void 회원정보_수정() throws Exception {
+        String profileUrl = "https://aws-s3.com";
+        String content = "안녕하세요";
+        List<String> tagList = Arrays.asList("001001", "001002");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserUpdateRequest request = UserUpdateRequest
+                .builder()
+                .nickname("updateNickname")
+                .content(content)
+                .tagList(tagList)
+                .profileUrl(profileUrl)
+                .build();
+
+        when(userService.updateUser(request, authentication)).thenReturn(mock(UserInfoResponse.class));
+
+        mockMvc.perform(patch("/api/v1/users/me")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        ).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 회원정보_수정시_유효하지_않은_사용자() throws Exception {
+        String profileUrl = "https://aws-s3.com";
+        String content = "안녕하세요";
+        List<String> tagList = Arrays.asList("001001", "001002");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserUpdateRequest request = UserUpdateRequest
+                .builder()
+                .nickname("updateNickname")
+                .content(content)
+                .tagList(tagList)
+                .profileUrl(profileUrl)
+                .build();
+
+        when(userService.updateUser(request, authentication)).thenThrow(new DodalApplicationException(ErrorCode.INVALID_TOKEN));
+
+        mockMvc.perform(patch("/api/v1/users/me")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        ).andDo(print()).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void 회원탈퇴() throws Exception {
+        mockMvc.perform(delete("/api/v1/users/me"))
+                .andDo(print()).andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 회원탈퇴_유효하지_않은_사용자() throws Exception {
+        mockMvc.perform(delete("/api/v1/users/me"))
+                .andDo(print()).andExpect(status().isUnauthorized());
+    }
+}
---
+++ src/test/java/com/dodal/meet/service/UserServiceTest.java
@@ -0,0 +1,120 @@
+package com.dodal.meet.service;
+
+import com.dodal.meet.controller.request.user.UserSignInRequest;
+import com.dodal.meet.controller.request.user.UserSignUpRequest;
+import com.dodal.meet.controller.response.user.UserSignInResponse;
+import com.dodal.meet.exception.DodalApplicationException;
+import com.dodal.meet.exception.ErrorCode;
+import com.dodal.meet.fixture.UserEntityFixture;
+import com.dodal.meet.model.SocialType;
+import com.dodal.meet.model.entity.TagEntity;
+import com.dodal.meet.model.entity.TokenEntity;
+import com.dodal.meet.model.entity.UserEntity;
+import com.dodal.meet.repository.TagEntityRepository;
+import com.dodal.meet.repository.TokenEntityRepository;
+import com.dodal.meet.repository.UserEntityRepository;
+import com.dodal.meet.repository.UserTagEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import java.util.Arrays;
+import java.util.Optional;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserEntityRepository userEntityRepository;
+
+    @MockBean
+    private UserTagEntityRepository userTagEntityRepository;
+
+    @MockBean
+    private TokenEntityRepository tokenEntityRepository;
+
+    @MockBean
+    private TagEntityRepository tagEntityRepository;
+
+    private static final String SOCIAL_ID ="123456789";
+    private static final SocialType SOCIAL_TYPE = SocialType.KAKAO;
+
+    @Test
+    void 회원가입이_정상적으로_동작하는_경우() {
+
+        when(userEntityRepository.findBySocialIdAndSocialType(SOCIAL_ID, SOCIAL_TYPE)).thenReturn(Optional.empty());
+        UserEntity userEntity = UserEntityFixture.getUserEntity(SOCIAL_ID, SOCIAL_TYPE);
+        when(userEntityRepository.save(any())).thenReturn(userEntity);
+        TagEntity tagEntity = TagEntity.builder().name("체중 관리").tagValue("001001").build();
+        when(tagEntityRepository.findByTagValue(any())).thenReturn(Optional.of(tagEntity));
+        UserSignUpRequest userSignUpRequest = UserSignUpRequest.builder()
+                .socialId(SOCIAL_ID)
+                .socialType(SOCIAL_TYPE)
+                .nickname("nickname")
+                .profileUrl("imgUrl")
+                .content("content")
+                .email("sss@gmail.com")
+                .tagList(Arrays.asList("001001"))
+                .build();
+
+        Assertions.assertDoesNotThrow(() ->userService.signUp(userSignUpRequest));
+    }
+
+    @Test
+    void 이미_가입한_회원이_가입하는_경우_예외를_반환한다() {
+        UserEntity fixture = UserEntityFixture.getUserEntity(SOCIAL_ID, SOCIAL_TYPE);
+
+        when(userEntityRepository.findBySocialIdAndSocialType(SOCIAL_ID, SOCIAL_TYPE)).thenReturn(Optional.of(fixture));
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(fixture));
+        UserSignUpRequest userSignUpRequest = UserSignUpRequest.builder()
+                .socialId(SOCIAL_ID)
+                .socialType(SOCIAL_TYPE)
+                .nickname("nickname")
+                .profileUrl("imgUrl")
+                .content("content")
+                .email("sss@gmail.com")
+                .tagList(Arrays.asList("001001"))
+                .build();
+        DodalApplicationException e = Assertions.assertThrows(DodalApplicationException.class, () -> userService.signUp(userSignUpRequest));
+
+        Assertions.assertEquals(ErrorCode.INVALID_SIGNUP_REQUEST, e.getErrorCode());
+    }
+
+    @Test
+    void 로그인이_정상적으로_동작하는_경우() {
+        UserSignUpRequest userSignUpRequest = UserSignUpRequest.builder().socialId(SOCIAL_ID).socialType(SOCIAL_TYPE).build();
+        UserSignInRequest userSignInRequest = UserSignInRequest.builder().socialId(SOCIAL_ID).socialType(SOCIAL_TYPE).build();
+
+        TokenEntity tokenEntity = TokenEntity.builder().fcmToken("token").refreshToken("token").build();
+
+        UserEntity fixture = UserEntity.SignUpDtoToEntity().request(userSignUpRequest)
+                .tokenEntity(tokenEntity).build();
+
+        when(tokenEntityRepository.findByUserEntity(any())).thenReturn(Optional.of(tokenEntity));
+        when(userEntityRepository.findBySocialIdAndSocialType(SOCIAL_ID, SOCIAL_TYPE)).thenReturn(Optional.of(fixture));
+        UserSignInResponse userSignInResponse = userService.signIn(userSignInRequest);
+
+        Assertions.assertDoesNotThrow(() -> userSignInResponse);
+        Assertions.assertEquals(userSignInResponse.getIsSigned(), "true");
+    }
+
+
+    @Test
+    void 로그인시_userName으로_회원가입한_유저가_없는_경우() {
+        when(userEntityRepository.findBySocialIdAndSocialType(SOCIAL_ID, SOCIAL_TYPE)).thenReturn(Optional.empty());
+
+        UserSignInRequest userSignInRequest = UserSignInRequest.builder().socialId(SOCIAL_ID).socialType(SOCIAL_TYPE).build();
+
+        UserSignInResponse userSignInResponse = userService.signIn(userSignInRequest);
+        Assertions.assertDoesNotThrow(() -> userSignInResponse);
+        Assertions.assertEquals(userSignInResponse.getIsSigned(), "false");
+    }
+}
