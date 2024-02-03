a97d3beb3f717f5d66192d3b0a8ab3e9d9e4f806
sasca37 <sasca37@naver.com>
2024-02-03 16:08:01 +0900

    refactor: #116 - 운영 환경 Swagger 제외

--- src/main/java/com/dodal/meet/configuration/SwaggerConfig.java
+++ src/main/java/com/dodal/meet/configuration/SwaggerConfig.java
@@ -9,8 +9,10 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile({"local", "dev"})
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI(@Value("${project.version}") String version,
